4e341545dda348a7c2d25032ee338946b574dced
Alfred Swinton <alfredswinton@gmail.com>
2016-12-02 18:06:01 -0800

    final commit

--- math.php
+++ math.php
@@ -1,13 +1,10 @@
 <?php 
 session_start();
 
-if (isset($_POST['guess'])) {
+/*if (isset($_POST['guess'])) {
     $guess = $_POST['guess'];
-}
+}*/
     
-$wins = 0;
-$count = 0;
-
 if (!(isset($_SESSION['wins']))){
 $_SESSION['wins'] = 0;
 }
@@ -16,15 +13,27 @@ $_SESSION['count'] = 0;
 }
 
 
-$num1 = rand(0,10);
+$num1 = rand(0,20);
 $sign = rand(0,1);
-$num2 = rand(0,10);
+$num2 = rand(0,20);
 if($sign == 0) {
     $dsign = "+";
     $solution = $num1 + $num2;
 } else {
     $dsign = "-";
     $solution = $num1 - $num2;
+}
+$_SESSION['correct'] = "";
+
+if (!empty($_POST['guess'])) {    
+if ($_POST['guess'] == $_SESSION['psolution']) {
+    $_SESSION['wins'] += 1;
+    $_SESSION['count'] += 1;
+    $_SESSION['correct'] = "You got it right!";
+} else {
+    $_SESSION['count'] += 1;
+    $_SESSION['correct'] = "Incorrect";
+}
 }
 ?>
 
@@ -47,25 +56,19 @@ echo $num1 . " " . $dsign . " " . $num2
     <h6>Score:</h6>
 <?php 
     echo $_SESSION['wins'] . "/" . $_SESSION['count'];
+    echo "<hr />";
+    if (isset($_SESSION['correct'])){
+    echo $_SESSION['correct'];
+    }
     ?>
 </body>
 </html>
 
 <?php
-if(isset($guess)) {
-    if($guess == $solution) {
-        $wins += 1;  
-        $count += 1;
-    } else {
-        $count += 1;
-    }
-}
 
-$_SESSION['wins'] += $wins;
-$_SESSION['count'] += $count;
 
-echo $guess;
-echo $solution;
+$_SESSION['psolution'] = $solution;
+
 #redirect to login screen if logout button pressed
 if (isset($_POST['logout'])) {
     session_destroy();
